ee9d487f131adce1e4adb7b1c072be0bcc5a09e3
sarathchan <sarathchan20@gmail.com>
2025-09-07 23:20:25 +0530

    analytics and speed insights added from vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import App from './App';
 import 'antd/dist/reset.css';
 import './index.css';
 import '@fontsource/roboto';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
+
